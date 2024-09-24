e68d3d310f57f65144eb2e737ae7a78d7131249b
Telman Safarli <tseferli0011@gmail.com>
2024-09-24 14:46:23 +0000

    weather

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
 "use strict";
 $(document).ready(function () {
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric`;
-    let apiId = `7b3a1a891bc9994755b43204e7adb60e`;
-    async function giveData(inValue) {
 
-        if (!$.trim($('.inpBox').val()) == "") {
-            let res = await fetch(apiUrl + `&appid=${apiId}&q=${inValue}`);
-            let data = await res.json();
-            console.log(res);
-            if(res.status == 200){
-                $('#city').html(data.name);
-                $('#temp').html(data.main.temp);
-                $('#like').html(data.main.feels_like);
+    $(document).on('keypress', function (e) {
+        if (e.which == 13) {
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&units=metric&appid=25b7f92099d395a9faf67ed8e74f03d5`;
+            async function myweather() {
+                let responce = await fetch(url);
+                let data = await responce.json();
+                $("#city").html(data.name);
+                $("#temp").html(data.main.temp);
+                $("#feels").html(data.main.feels_like);
+                $("myWeather").val("");
+                console.log(data);
             }
-            else{
-                return;
-            }
-           
+            myweather();
         }
-    }
-    $('.btn').click(function (e) {
+
+    });
+
+    $("#btn").click(function (e) {
         e.preventDefault();
-        giveData($('.inpBox').val());
-        $('.inpBox').val(" ");
-    });
-    $('.inpBox').keypress(function (e) {
-        if (e.keyCode == 13) {
-            giveData($('.inpBox').val());
-            $('.inpBox').val(" ");
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&units=metric&appid=25b7f92099d395a9faf67ed8e74f03d5`
+        async function myweather() {
+            let responce = await fetch(url);
+            let data = await responce.json();
+            $("#city").html(data.name);
+            $("#temp").html(data.main.temp);
+            $("#feels").html(data.main.feels_like);
+            $("myWeather").val("");
+            console.log(data);
         }
+        myweather();
     });
-
+    
 });
